b4a91b73773600d2ef81be0235567c2d300d872b
Maurycy Szmurlo <mszmurlo@buguigny.org>
2025-06-01 13:25:25 +0300

    Updated mix with 'package()' so that it can be published on Hex

--- mix.exs
+++ mix.exs
@@ -7,7 +7,18 @@ defmodule Calc.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs",],
+      maintainers: ["Maurycy Szmurlo"],
+      description: "Dummy package for library testing purpose",
+      licenses: ["MIT License"],
+      links: %{"GitHub" => "https://github.com/mszmurlo/calc"}
     ]
   end
   
@@ -21,6 +32,7 @@ defmodule Calc.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false, warn_if_outdated: true},
     ]
   end
 end
